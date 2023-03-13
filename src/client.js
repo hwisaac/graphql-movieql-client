b20c77e715dcc820d2b1109caf8eb800fa57863d
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-13 16:34:38 +0900

    apollo client setup

--- src/client.js
+++ src/client.js
@@ -1,13 +1,3 @@
-# graphql tutorial
-
-## apollo client
-
-> ReactJS 의 apollo client 가 graphql 서버와 통신하도록 구성하자.
-
-graphql 의 api 의 url 을 써서 client를 구성할 수 있다.
-
-```js
-// client.js
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
@@ -28,8 +18,3 @@ client
   .then((data) => console.log(data));
 
 export default client;
-```
-
-```js
-// index.js
-```
